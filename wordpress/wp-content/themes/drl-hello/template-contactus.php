e8bfafb63d3dc305a94f3a4c0888bc6ad7a3a581
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2022-04-19 02:23:57 +0700

    init template-contactus

---
+++ wordpress/wp-content/themes/drl-hello/template-contactus.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template Name: Contact Us
+ */
+?>
+
+<?php get_header(); ?>
+<div class="container">
+    <h1><?php the_title(); ?></h1>
+    
+    <div class="row">
+        <div class="col">
+            jadi ceritanya ini teh ada teksnya
+        </div>
+        <div class="col">
+            <?php get_template_part("includes/section", "content"); ?>
+        </div>
+    </div>
+</div>
+<?php get_footer(); ?>
