78eb1d3a4efd636c9e292933bf46912e3ac5bae6
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2022-04-19 23:13:09 +0700

    update section-archive: update layout

--- wordpress/wp-content/themes/drl-hello/includes/section-archive.php
+++ wordpress/wp-content/themes/drl-hello/includes/section-archive.php
@@ -1,9 +1,13 @@
 <?php if (have_posts()):
   while (have_posts()):
     the_post(); ?>
-    <h1><?php the_title(); ?></h1>
-
-    <?php the_content(); ?>
+    <div class="card mb-2">
+        <div class="card-body">
+            <h2 class="card-title"><?php the_title(); ?></h2>
+            <?php the_excerpt(); ?>
+            <a class="btn btn-primary" href="<?php the_permalink(); ?>">Read more...</a>
+        </div>
+    </div>
 <?php
   endwhile;
 endif; ?>
