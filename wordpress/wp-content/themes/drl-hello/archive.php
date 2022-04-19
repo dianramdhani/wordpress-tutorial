a29d31bc2c4ad88026dffb58569120cb3afc6460
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2022-04-19 23:12:16 +0700

    update archive: implement simple pagination

--- wordpress/wp-content/themes/drl-hello/archive.php
+++ wordpress/wp-content/themes/drl-hello/archive.php
@@ -1,5 +1,8 @@
 <?php get_header(); ?>
 <div class="container">
     <?php get_template_part("includes/section", "archive"); ?>
+    
+    <?php previous_posts_link(); ?>
+    <?php next_posts_link(); ?>
 </div>
 <?php get_footer(); ?>
